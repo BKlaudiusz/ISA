eac007910ec5ba693ce381128c296e6d94507226
Klaudiusz <klaudiusz.boblack@gmail.com>
2021-03-25 14:55:10 +0100

    init repo

---
+++ controllers/samochodzik/build/release/samochodzik.d
@@ -0,0 +1,14 @@
+build/release/samochodzik.o: samochodzik.c \
+ E:\Webots/include/controller/c/webots/camera.h \
+ E:\Webots/include/controller/c/webots/camera_recognition_object.h \
+ E:\Webots/include/controller/c/webots/types.h \
+ E:\Webots/include/controller/c/webots/device.h \
+ E:\Webots/include/controller/c/webots/nodes.h \
+ E:\Webots/include/controller/c/webots/display.h \
+ E:\Webots/include/controller/c/webots/gps.h \
+ E:\Webots/include/controller/c/webots/keyboard.h \
+ E:\Webots/include/controller/c/webots/lidar.h \
+ E:\Webots/include/controller/c/webots/lidar_point.h \
+ E:\Webots/include/controller/c/webots/robot.h \
+ E:\Webots/include/controller/c/webots/vehicle/driver.h \
+ E:\Webots/include/controller/c/webots/types.h
